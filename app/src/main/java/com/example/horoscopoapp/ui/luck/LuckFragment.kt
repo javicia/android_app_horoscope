3bcb56c42ad43ca271cecc1dc4f7f77d59d113a8
Javier García <javigpdeveloper@gmail.com>
2023-12-27 18:05:28 +0100

    SPINROULETTE

--- app/src/main/java/com/example/horoscopoapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscopoapp/ui/luck/LuckFragment.kt
@@ -1,5 +1,6 @@
 package com.example.horoscopoapp.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.horoscopoapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.random.Random
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
@@ -14,10 +16,30 @@ class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.ivRoulette.setOnClickListener {
+            spinRoulette()
+        }
+    }
+
+    private fun spinRoulette() {
+
+        val animator = ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View{
+    ): View {
         _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
         return binding.root
     }

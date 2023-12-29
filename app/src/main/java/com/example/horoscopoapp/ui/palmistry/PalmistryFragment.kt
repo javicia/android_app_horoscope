268867b2b3f5790edc907df56707d3b0a3f59a9b
Javier García <javigpdeveloper@gmail.com>
2023-12-29 19:26:43 +0100

    permission management

--- app/src/main/java/com/example/horoscopoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
@@ -15,29 +16,42 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object{
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
     private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission())
+        ActivityResultContracts.RequestPermission()
+    )
     { isGranted ->
-        if(isGranted){
-
+        if (isGranted) {
+            //startcamera
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "Acepta los permisos para poder disfrutar de una experiencia mágica.",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if(checkCameraPermission()){
+        if (checkCameraPermission()) {
 
-        }else{
-
+        } else {
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
     }
 
-    fun checkCameraPermission():Boolean{
-        return PermissionChecker.checkSelfPermission(requireContext(),
-            android.Manifest.permission.CAMERA) == PermissionChecker.PERMISSION_GRANTED
+    fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(),
+            CAMERA_PERMISSION
+        ) == PermissionChecker.PERMISSION_GRANTED
     }
 
     override fun onCreateView(

11cb012bb7f618a19f426508aeb50e1f66081eb2
Javier García <javigpdeveloper@gmail.com>
2023-12-29 06:36:29 +0100

    permisos camara

--- app/src/main/java/com/example/horoscopoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -1,10 +1,14 @@
 package com.example.horoscopoapp.ui.palmistry
 
+import android.app.Instrumentation.ActivityResult
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
+import com.example.horoscopoapp.Manifest
 import com.example.horoscopoapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +17,28 @@ class PalmistryFragment : Fragment() {
 
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission())
+    { isGranted ->
+        if(isGranted){
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(checkCameraPermission()){
+
+        }else{
+
+        }
+    }
+
+    fun checkCameraPermission():Boolean{
+        return PermissionChecker.checkSelfPermission(requireContext(),
+            android.Manifest.permission.CAMERA) == PermissionChecker.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
